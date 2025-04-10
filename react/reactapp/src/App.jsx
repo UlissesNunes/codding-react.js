7b736f38739e1617744cc94d10225a24afce615e
UlissesNunes <ulissesnunesx@gmail.com>
2025-04-10 11:40:29 -0300

    inicializei estudos pesado com o react + vite

---
+++ react/reactapp/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>hello,World</h1>
+      <p>Este é meu código no curso da udemy, vou relembrar os conceitos de react que já tinha estudado o bom é que eu tou relelmbrando mais rápido e iniciei com vite</p>
+    </>
+  )
+
+}
+export default App
