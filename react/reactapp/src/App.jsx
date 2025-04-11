1e728f6973d6e63bf84273e58fe27bfa01bd5cef
UlissesNunes <ulissesnunesx@gmail.com>
2025-04-11 11:57:24 -0300

    ok

--- react/reactapp/src/App.jsx
+++ react/reactapp/src/App.jsx
@@ -8,8 +8,14 @@ function App() {
     <>
       <h1>hello,World</h1>
       <p>Este é meu código no curso da udemy, vou relembrar os conceitos de react que já tinha estudado o bom é que eu tou relelmbrando mais rápido e iniciei com vite</p>
+      <p>Esse é o meu primeiro projeto com vite</p>
+      
     </>
   )
 
 }
 export default App
+
+// use o rafce para criar funções
+// use o rafcp para criar funções com props
+// use o rafc para criar funções com exportação padrão
