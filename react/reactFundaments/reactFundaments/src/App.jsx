50b30480d65da897add41b0815ce5a791b8fa10f
UlissesNunes <ulissesnunesx@gmail.com>
2025-05-29 09:32:00 -0300

    update

---
+++ react/reactFundaments/reactFundaments/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1>NOVA FASE</h1>
+     <p>Objetivo central = Dominar react e tailwind e fazer meu portifólio inteiro com eles e ter ao menos 5 grandes projetos</p> <p>Sendo 2 deles com REACT/TAILWIND/TYPESCRIPT</p>
+    </>
+  )
+}
+
+export default App
