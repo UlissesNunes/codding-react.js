a8b7b294ecea7f6390720d269030ee6a94b976e4
UlissesNunes <ulissesnunesx@gmail.com>
2025-05-30 21:53:48 -0300

    Update

--- react/challange1/challange1/src/App.jsx
+++ react/challange1/challange1/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-       <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+       <div className="min-h-screen bg-gray-200 flex items-center justify-center">
       <h1 className="text-2xl font-bold text-blue-600">DIFICINNN MAS TAILWIND COM REACT É REALIDADEEEEE!</h1> </div>
       <p className='bg-slate-700 hover:bg-gray-500'>MAS VOU PEGAR O JEITO</p>
    
